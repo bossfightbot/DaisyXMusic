17b5fc589e87bf4e328c4f90219b8d5c4b26251d
bossfightbot <82875938+bossfightbot@users.noreply.github.com>
2021-04-23 14:25:03 -0700

    Update userbotjoin.py

--- handlers/userbotjoin.py
+++ handlers/userbotjoin.py
@@ -20,7 +20,7 @@ async def addchannel(client, message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name =  "DaisyMusic"
+        user.first_name =  "XVIImusic"
 
     try:
         await USER.join_chat(invitelink)
@@ -33,7 +33,7 @@ async def addchannel(client, message):
         print(e)
         await message.reply_text(
             f"<b>User {user.first_name} couldn't join your group! Make sure user is not banned in group."
-            "\n\nOr manually add @DaisyXhelper to your Group and try again</b>",
+            "\n\nOr manually add @lifeisuckx to your Group and try again</b>",
         )
         return
     await message.reply_text(
